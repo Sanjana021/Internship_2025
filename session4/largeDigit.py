9728c3635dbbb3da8522afe6b2d03167c381362f
Sanjana G Kundar <sanjanakundar57@gmail.com>
2025-02-23 09:10:24 +0530

    program one more

---
+++ session4/largeDigit.py
@@ -0,0 +1,12 @@
+""" Find Largest Digit:
+Create a Python program to find the largest digit in a given list using a while loop."""
+
+listA=[24,12,21,16,57]
+large=listA[0]
+index=1
+while index<len(listA):
+    if listA[index]>large:
+        large=listA[index]
+    index+=1
+print("largest sigit is:",large)
+    
